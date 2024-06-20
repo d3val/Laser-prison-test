52f5ca5f6b8321fb613a898948d7cc912b4ee012
d3val <devalt5008@gmail.com>
2024-06-20 11:48:57 -0600

    Many lasers per shoot

--- Assets/Scripts/LaserManager.cs
+++ Assets/Scripts/LaserManager.cs
@@ -6,34 +6,39 @@ public class LaserManager : MonoBehaviour
 {
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float shootRate = 2.5f;
-    private int lasersPerShoot = 1;
+    private int lasersPerShoot = 3;
 
     private Vector2 spawnLimits = new Vector2(-14, 15);
     private Vector2 rotationLimits = new Vector2(50, 180);
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(ShootLaser());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     IEnumerator ShootLaser()
     {
+        int xPos;
+        int yPos;
+        float eulerY;
+        float eulerX;
+        
         yield return new WaitForSeconds(shootRate);
-        int xPos = Random.Range((int)spawnLimits.x, (int)spawnLimits.y);
-        int yPos = Random.Range((int)spawnLimits.x, (int)spawnLimits.y);
 
-        float eulerX = Random.Range(-rotationLimits.x, rotationLimits.x);
-        float eulerY = Random.Range(-rotationLimits.y, rotationLimits.y);
-        Quaternion laserRotation = Quaternion.Euler(eulerX, eulerY, 0);
+        for (int i = 0; i < lasersPerShoot; i++)
+        {
+            xPos = Random.Range((int)spawnLimits.x, (int)spawnLimits.y);
+            yPos = Random.Range((int)spawnLimits.x, (int)spawnLimits.y);
 
-        Vector3 laserPosition = new Vector3(xPos, 0, yPos);
-        Instantiate(laserPrefab, laserPosition, laserRotation);
+            eulerX = Random.Range(-rotationLimits.x, rotationLimits.x);
+            eulerY = Random.Range(-rotationLimits.y, rotationLimits.y);
+            Quaternion laserRotation = Quaternion.Euler(eulerX, eulerY, 0);
+
+            Vector3 laserPosition = new Vector3(xPos, 0, yPos);
+            Instantiate(laserPrefab, laserPosition, laserRotation);
+        }
         StartCoroutine(ShootLaser());
     }
 }
