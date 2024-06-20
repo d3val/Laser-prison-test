d03177174c803ead60d22f874c128407a10b023a
Eduardo Alvarez <90423330+d3val@users.noreply.github.com>
2024-06-20 13:14:13 -0600

    Game manager (#2)
    * Score and end of the game added
    
    * Game over UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //Game variables
+    [Header("Game parameters")]
+    [SerializeField] int playerLifes = 3;
+    private int currentScore = 0;
+    private int scoreMultiplier = 1;
+    private bool isPlaying = true;
+    private float rawScore;
+
+    [Header("UI")]
+    //UI variables
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI multiplierText;
+    [SerializeField] List<GameObject> lifeIcons;
+    [SerializeField] GameObject GameOverPanel;
+    [SerializeField] TextMeshProUGUI FinalScoreText;
+    private int lifeIconsIndex = 0;
+
+    // Class instance
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+
+        Time.timeScale = 1;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(IncreaseDificulty(15, 2));
+        StartCoroutine(IncreaseDificulty(40, 3));
+        StartCoroutine(IncreaseDificulty(90, 5));
+    }
+
+    private void Update()
+    {
+        if (!isPlaying)
+            return;
+        
+        //When player loses her lifes, the game is over.
+        if (playerLifes <= 0)
+            EndGame();
+
+        //Score calculation
+        rawScore += Time.deltaTime * scoreMultiplier;
+        currentScore = (int)rawScore;
+        scoreText.text = currentScore.ToString();
+    }
+
+    //Sets some parameters to end the game
+    private void EndGame()
+    {
+        isPlaying = false;
+        GameOverPanel.SetActive(true);
+        FinalScoreText.text = currentScore.ToString();
+        Time.timeScale = 0;
+    }
+
+    //Used to increase the difficulty of the game
+    private IEnumerator IncreaseDificulty(float time, int newScoreMultiplier)
+    {
+        yield return new WaitForSeconds(time);
+        scoreMultiplier = newScoreMultiplier;
+        multiplierText.text = "X" + newScoreMultiplier;
+    }
+
+    //Substacts one life.
+    public void DecreaseLife()
+    {
+        if(lifeIconsIndex >= lifeIcons.Count)
+        {
+            Debug.LogError("lifeIconsIndex out of range");
+            return;
+        }
+
+        playerLifes--;
+        lifeIcons[lifeIconsIndex].SetActive(false);
+        lifeIconsIndex++;
+    }
+
+    //Restarts the scene to its original state.
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
